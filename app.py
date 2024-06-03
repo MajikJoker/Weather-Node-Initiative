68523748f84ef2acec7212a23889b292a3357aa7
MajikJoker <33824823+MajikJoker@users.noreply.github.com>
2024-06-04 00:16:50 +0800

    Update

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 # MongoDB connection
 mongo_uri = os.environ.get('MONGO_URI')
 client = MongoClient(mongo_uri)
-db = client.userManagement
-users_collection = db.users
+db = client["database_name"]
+users = db["users"]
 
 # Routes
 @app.route('/')
@@ -20,17 +23,16 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Handle login logic
         username = request.form.get('username')
         password = request.form.get('password')
         user = users.find_one({"username": username})
-        
+
         if user and check_password_hash(user['password'], password):
             return redirect(url_for('loggedhome'))
         else:
             flash("Invalid username or password")
             return redirect(url_for('login'))
-            
+
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -39,18 +41,16 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        # Check if user already exists
         if users.find_one({"username": username}):
             flash("Username already exists")
             return redirect(url_for('register'))
-        
-        # Hash the password and insert into the database
+
         hashed_password = generate_password_hash(password, method='sha256')
         user_data = {
             "username": username,
             "password": hashed_password
         }
-        
+
         try:
             users.insert_one(user_data)
             flash("Registration successful! You can now log in.")
@@ -58,11 +58,11 @@ def register():
         except Exception as e:
             flash(f"An error occurred: {str(e)}")
             return redirect(url_for('register'))
-            
+
     return render_template('register.html')
 
 @app.route('/loggedhome')
-def logged_home():
+def loggedhome():
     return render_template('loggedhome.html')
 
 if __name__ == '__main__':
